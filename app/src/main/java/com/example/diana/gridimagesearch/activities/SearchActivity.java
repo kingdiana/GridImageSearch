147b4a2ba40e816abf264bf9d68969651c2848d3
Diana King <king_diana@yahoo.com>
2015-03-04 01:32:34 -0800

    Reverted minSDK change.  Added preferences.  Played with the infinite scrolling to remove earlier hack that ensured first screen of results was full despite 8 image limit.  The scrolling is sort of working, but not exactly as it should be (e.g. does not distinguish between scrolling off top vs. bottom).

--- app/src/main/java/com/example/diana/gridimagesearch/activities/SearchActivity.java
+++ app/src/main/java/com/example/diana/gridimagesearch/activities/SearchActivity.java
@@ -1,7 +1,9 @@
 package com.example.diana.gridimagesearch.activities;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
@@ -29,12 +31,16 @@ import java.util.ArrayList;
 
 public class SearchActivity extends ActionBarActivity {
     private EditText etQuery;
-    private String previousQuery = "";
     private GridView gvResults;
     private Button btnSearch;
     private ArrayList<ImageResult> imageResult;
     private ImageResultsAdapter aImageResults;
     int count = 0;
+    Boolean newSearch = false;
+    private String size;
+    private String site;
+    private String type;
+    private String color;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,6 +59,8 @@ public class SearchActivity extends ActionBarActivity {
             private void isScrollCompleted() {
                 if (this.currentVisibleItemCount > 0 && this.currentScrollState == SCROLL_STATE_IDLE) {
                     /* Scroll has completed, do the work. */
+                    onImageSearch(getCurrentFocus());
+                } else if (this.currentFirstVisibleItem < 16) {
                     onImageSearch(getCurrentFocus());
                 }
             }
@@ -95,37 +103,78 @@ public class SearchActivity extends ActionBarActivity {
         return true;
     }
 
+    public void onSearchButtonClicked(View v) {
+        newSearch = true;
+        onImageSearch(v);
+    }
+
     public void onImageSearch(View v) {
         final String query = etQuery.getText().toString();
-        if (!query.contentEquals(previousQuery)) {
-            count = 0;
-            previousQuery = query;
-            aImageResults.clear();
-        }
         AsyncHttpClient client = new AsyncHttpClient();
         // https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=android&rsz=8
-        String url = "https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=" +
-                     query + "&rsz=8&start=" + count;
-        client.get(url, new JsonHttpResponseHandler() {
+        String url = "https://ajax.googleapis.com/ajax/services/search/images?v=1.0&rsz=8";
+        String urlWithCount;
+
+        if (newSearch == true) {
+            count = 0;
+            aImageResults.clear();
+            newSearch = false;
+            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+            size = prefs.getString(getString(R.string.pref_size_key),
+                    getString(R.string.pref_size_default));
+            site = prefs.getString(getString(R.string.pref_site_key),
+                    getString(R.string.pref_site_default));
+            color = prefs.getString(getString(R.string.pref_color_key),
+                    getString(R.string.pref_color_default));
+            type = prefs.getString(getString(R.string.pref_type_key),
+                    getString(R.string.pref_type_default));
+        }
+        if (!size.contentEquals("none")) url += "&imgsz=" + size;
+        if (!color.contentEquals("none")) url += "&imgcolor=" + color;
+        if (!type.contentEquals("none")) url += "&imgtype=" + type;
+        if (!site.contentEquals("")) url += "&as_sitesearch=" + site;
+        url += "&q=" + query;
+        urlWithCount = url + "&start=" + count;
+
+        if (count >= 64) return; // API limits results to 64 images.
+        Log.d(">>>> URL is ", urlWithCount);
+        client.get(urlWithCount, new JsonHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-                if (count < 64) {  // api limits results to 64 total
-                    Log.d("DEBUG", response.toString());
-                    try {
-                        JSONObject jsonObject = response.getJSONObject("responseData");
-                        JSONArray imageResultJson = jsonObject.getJSONArray("results");
-                        aImageResults.addAll(ImageResult.fromJSONArray(imageResultJson));
-                        count += imageResultJson.length();
-
-                        if (count < 16 && imageResultJson.length() >= 8) {
-                            btnSearch.callOnClick();
-                        }
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
+                Log.d("DEBUG", response.toString());
+                try {
+                    JSONObject jsonObject = response.getJSONObject("responseData");
+                    JSONArray imageResultJson = jsonObject.getJSONArray("results");
+                    aImageResults.addAll(ImageResult.fromJSONArray(imageResultJson));
+                    count += imageResultJson.length();
+                } catch (JSONException e) {
+                    e.printStackTrace();
                 }
             }
         });
+/*
+        // If count==0, there were no hits and no need to search more.
+        // If count>16, we've filled the first screen.
+        if (count == 0 || count > 16) return;
+        // else do another search to fill out the initial screen.
+
+        url = urlWithCount + "&start=" + count;
+        Log.d(">>>> URL is ", urlWithCount);
+        client.get(urlWithCount, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                Log.d("DEBUG", response.toString());
+                try {
+                    JSONObject jsonObject = response.getJSONObject("responseData");
+                    JSONArray imageResultJson = jsonObject.getJSONArray("results");
+                    aImageResults.addAll(ImageResult.fromJSONArray(imageResultJson));
+                    count += imageResultJson.length();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        */
     }
 
     @Override
@@ -137,6 +186,7 @@ public class SearchActivity extends ActionBarActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            startActivity(new Intent(this, SettingsActivity.class));
             return true;
         }
 
